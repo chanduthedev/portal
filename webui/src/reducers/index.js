7536c44d4a042082eb635d6052488cdd3f83c59a
Chandra Pasumarthi <chanduthedev@gmail.com>
2021-09-21 11:43:20 +0800

    file name typo fixed

--- webui/src/reducers/index.js
+++ webui/src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import signUpReducer from "./signUpReducer";
+import signUpReducer from "./SignUpReducer";
 import loginReducer from "./loginReducer";
 const rootReducers = combineReducers({
   signUp: signUpReducer,
